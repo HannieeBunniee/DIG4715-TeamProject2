cbd0f2d427adcc0c324e3bfae8da9bc98b7d99ee
HannieeBunniee <54680570+hanniee227@users.noreply.github.com>
2020-02-12 20:47:46 -0500

    First Person Player Sample Added
    First person added but the code for the player make the mouse stop working for pause menu..

---
+++ HallowWay/Assets/Script/GameControllerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // to use UI text
+
+public class GameControllerScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    //=========Updates===========
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Debug.Log("Quitting Game");
+            Application.Quit();
+        }
+    }
+}
